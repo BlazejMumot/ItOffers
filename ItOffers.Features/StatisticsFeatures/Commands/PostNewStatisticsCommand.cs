b63e1d3caf64f4b7721e50c4b569c77b620b71ef
Błażej Mumot <blmum@dfds.com>
2022-11-18 10:48:12 +0100

    feat: stats for other offers

--- ItOffers.Features/StatisticsFeatures/Commands/PostNewStatisticsCommand.cs
+++ ItOffers.Features/StatisticsFeatures/Commands/PostNewStatisticsCommand.cs
@@ -17,24 +17,34 @@ namespace ItOffers.Features.StatisticsFeatures.Commands
 
             public async Task<bool> Handle(PostNewStatisticsCommand request, CancellationToken cancellationToken)
             {
+                var scrapList = new List<StatisticsCollectionModel> {
+                    new StatisticsCollectionModel { Type = "Backend", CollectionName = "BackendOffers" },
+                    new StatisticsCollectionModel { Type = "Frontend", CollectionName = "FrontendOffers" },
+                    new StatisticsCollectionModel { Type = "Fullstack", CollectionName = "FullStackOffers" },
+                    new StatisticsCollectionModel { Type = "Mobile", CollectionName = "MobileOffers" }};
+
                 var database = _mongoClient.GetDatabase("ItScrapper");
-                var collection = database.GetCollection<GetOfferModel>("BackendOffers").AsQueryable<GetOfferModel>().ToArray();
-                var avgSalary = collection.Select(o => o.AvgSalary).Average();
-                var avgSeniority = collection.GroupBy(o => o.Seniority).Select(g => new { Key = g.Key, Count = g.Count() }).OrderByDescending(x => x.Count).Take(3).ToList();
-                var statCollection = database.GetCollection<StatisticsModel>("Statistics");
-                await statCollection.InsertOneAsync(new StatisticsModel()
+                foreach(var model in scrapList)
                 {
-                    Type = "Backend",
-                    Amount = collection.Length,
-                    AvgSalary = avgSalary,
-                    MostSeniorityOffers = avgSeniority[0].Key,
-                    MostSeniorityOffersCount = avgSeniority[0].Count,
-                    SecondSeniorityOffers = avgSeniority[1].Key,
-                    SecondSeniorityOffersCount = avgSeniority[1].Count,
-                    LeastSeniorityOffers = avgSeniority[2].Key,
-                    LeastSeniorityOffersCount = avgSeniority[2].Count,
-                    CreatedAt = DateTime.Now,
-                });
+                    var collection = database.GetCollection<GetOfferModel>(model.CollectionName).AsQueryable<GetOfferModel>().ToArray();
+                    var avgSalary = collection.Select(o => o.AvgSalary).Average();
+                    var avgSeniority = collection.GroupBy(o => o.Seniority).Select(g => new { Key = g.Key, Count = g.Count() }).OrderByDescending(x => x.Count).Take(3).ToList();
+                    var statCollection = database.GetCollection<StatisticsModel>("Statistics");
+                    await statCollection.InsertOneAsync(new StatisticsModel()
+                    {
+                        Type = model.Type,
+                        Amount = collection.Length,
+                        AvgSalary = avgSalary,
+                        MostSeniorityOffers = avgSeniority[0].Key,
+                        MostSeniorityOffersCount = avgSeniority[0].Count,
+                        SecondSeniorityOffers = avgSeniority[1].Key,
+                        SecondSeniorityOffersCount = avgSeniority[1].Count,
+                        LeastSeniorityOffers = avgSeniority[2].Key,
+                        LeastSeniorityOffersCount = avgSeniority[2].Count,
+                        CreatedAt = DateTime.Now,
+                    });
+                }
+
                 return true;
             }
         }
